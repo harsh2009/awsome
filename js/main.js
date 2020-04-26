5eada63f5acc2a62762f4dc58cb06585e185cecb
harsh kumar <harshkumar2009@gmail.com>
2020-04-26 09:00:29 +0530

    completed finished yey!!

--- js/main.js
+++ js/main.js
@@ -2,24 +2,19 @@ $(document).ready(function () {
 
     new WOW().init();
 
-    $(".burguger-nav").on('click',function(){
+
+    $(".burger-nav").on("click", function () {
+
         $(".container nav ul").toggleClass("open");
+
     });
 
-    $('.js-wp-2').waypoint(function (direction) {
-        $('.js-wp-2').addClass('animated slideInUp')
-    }, {
-        offset: '70%'
-    });
 
-    $('.js-wp-3').waypoint(function (direction) {
-        $('.js-wp-3').addClass('animated fadeIn')
-    }, {
-        offset: '70%'
-    });
-    $("#box").animate({height: "300px"});
 
-    $(".iPhone-power-button").delay(2300).animate( {botttom: "+=-3"}, 300);
-    $(".iPhone-power-button").delay(300).animate( {top: "+=-3"}, 100);
+
+    
+
+
+
 
 });
